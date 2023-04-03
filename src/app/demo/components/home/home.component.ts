99ba8909656a820ea06b7231cffb500a345f49f5
amina.yahia@eniso.u-sousse.tn <amina.yahia@eniso.u-sousse.tn>
2023-04-03 22:42:25 +0200

    [ADD] landing page and home page

--- src/app/demo/components/home/home.component.ts
+++ src/app/demo/components/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {LayoutService} from "../../../core/layout/service/app.layout.service";
 import {Router} from "@angular/router";
+import { PrimeNGConfig } from 'primeng/api';
+import { Image } from 'primeng/image';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+    providers:[ Image]
 })
 export class HomeComponent {
     constructor(public layoutService: LayoutService, public router: Router) { }
--- src/app/demo/components/home/home.module.ts
+++ src/app/demo/components/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import {HomeComponent} from "./home.component";
 import {PrimeNgUIComponentModule} from "../../../shared/modules/primeng-ui-component.module";
